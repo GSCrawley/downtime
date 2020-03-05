a544eb17997f862039e93d562748ce8b15a915b0
Jetpuffed <brandon@jetpuffed.org>
2020-03-05 11:36:48 -0800

    fixes

--- Downtime/users/apps.py
+++ Downtime/users/apps.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 class UsersConfig(AppConfig):
     name = "users"
-    verbose_name = _("Users")
+    # verbose_name = _("Users")
 
     def ready(self):
         try:
