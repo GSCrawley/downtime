e42dc82b5912f67b5ffc63b7ad67191ff1ae6abf
Jetpuffed <brandon@jetpuffed.org>
2020-03-05 12:44:50 -0800

    fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -68,8 +68,8 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "..users.apps.UsersConfig",
-    "..main.apps.MainConfig",
+    ".users.apps.UsersConfig",
+    ".main.apps.MainConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
