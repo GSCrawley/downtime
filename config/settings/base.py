370743ef21fda2c8b57e3bb08803b6b5798cb71c
Jetpuffed <brandon@jetpuffed.org>
2020-03-05 12:34:30 -0800

    fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -68,8 +68,8 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "downtime.users.apps.UsersConfig",
-    "downtime.main.apps.MainConfig",
+    "downtime.users",
+    "downtime.main",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
